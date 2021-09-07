a1ac4b8827793a21d2b724e80a9704da9e449ce6
emilbaluev <emil.baluev@gmail.com>
2021-09-08 02:51:05 +0300

    Fix try/except and params request

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def get_short_link(token, base_url, url_link):
 def get_total_clicks(token, base_url, link):
     short_link = link.split('//')[1]
     click_url = f'{base_url}/bitlinks/{short_link}/clicks/summary'
-    headers = {"Authorization": f"Bearer {token}", }
-    params = (('unit', 'day'), ('units', '-1'),)
+    headers = {"Authorization": f"Bearer {token}"}
+    params = {'unit': 'day', 'units': '-1'}
     click_response = requests.get(click_url, headers=headers, params=params)
     click_response.raise_for_status()
     return click_response.json()['total_clicks']
@@ -28,7 +28,7 @@ def get_total_clicks(token, base_url, link):
 
 def check_bitlink(token, base_url, link):
     bitlink_check_url = f"{base_url}bitlinks/{link}"
-    headers = {"Authorization": f"Bearer {token}", }
+    headers = {"Authorization": f"Bearer {token}",}
     response = requests.get(bitlink_check_url, headers=headers)
     return response.ok
 
@@ -37,33 +37,28 @@ def main():
     load_dotenv()
     token = os.getenv("ACCESS_TOKEN")
     base_url = 'https://api-ssl.bitly.com/v4/'
-    parser = argparse.ArgumentParser(description='Cчитает количество кликов или создает короткую ссылку')
+    parser = argparse.ArgumentParser(
+        description='Cчитает количество кликов или создает короткую ссылку')
     parser.add_argument('url', help='Ссылка для проверки')
     args = parser.parse_args()
     link = args.url
-    if link.startswith("https://bit.ly"):
-        try:
+    try:
+        if link.startswith("https://bit.ly"):
             bitlink_exist = check_bitlink(token, base_url, link)
             if bitlink_exist is False:
                 total_clicks = get_total_clicks(token, base_url, link)
                 print(f"По ссылке {link} перешли {total_clicks} раз(а).")
-        except requests.ConnectionError:
-            print("Сайт не отвечает.")
-        except KeyError:
-            print('Ошибка ввода ссылки')
 
-        except requests.HTTPError:
-            print("Ошибка сервиса Bitly")
-    else:
-        try:
+        else:
             bitlink = get_short_link(token, base_url, link)
-            print(f"Теперь для доступа к {link} Вы можете воспользоваться следующей ссылкой:\n{bitlink}")
-        except requests.ConnectionError:
-            print("Сайт не отвечает.")
-        except KeyError:
-            print('Ошибка ввода ссылки')
-        except requests.HTTPError:
-            print("Ошибка сервиса Bitly")
+            print(
+                f"Теперь для доступа к {link} Вы можете воспользоваться следующей ссылкой:\n{bitlink}")
+    except requests.ConnectionError:
+        print("Сайт не отвечает.")
+    except KeyError:
+        print('Ошибка ввода ссылки')
+    except requests.HTTPError:
+        print("Ошибка сервиса Bitly")
 
 
 if __name__ == "__main__":
