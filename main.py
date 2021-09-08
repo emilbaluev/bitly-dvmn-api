7511ebfa20fcccf4790b1ea9dd57b3d22159b589
emilbaluev <emil.baluev@gmail.com>
2021-09-08 03:18:01 +0300

    Add def is_bitlink

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv
 import argparse
+from urllib.parse import urlparse
 
 
 def get_short_link(token, base_url, url_link):
@@ -16,19 +17,24 @@ def get_short_link(token, base_url, url_link):
     return response.json()['link']
 
 
-def get_total_clicks(token, base_url, link):
-    short_link = link.split('//')[1]
+def get_total_clicks(headers, base_url, short_link):
     click_url = f'{base_url}/bitlinks/{short_link}/clicks/summary'
-    headers = {"Authorization": f"Bearer {token}"}
     params = {'unit': 'day', 'units': '-1'}
     click_response = requests.get(click_url, headers=headers, params=params)
     click_response.raise_for_status()
     return click_response.json()['total_clicks']
 
 
-def check_bitlink(token, base_url, link):
+def is_bitlink(base_url, headers, short_link):
+    response = requests.get(
+        "{}/bitlinks/{}".format(base_url, short_link),
+        headers=headers)
+    return response.ok
+
+
+def check_bitlink(headers, base_url, link):
     bitlink_check_url = f"{base_url}bitlinks/{link}"
-    headers = {"Authorization": f"Bearer {token}",}
+
     response = requests.get(bitlink_check_url, headers=headers)
     return response.ok
 
@@ -41,18 +47,22 @@ def main():
         description='Cчитает количество кликов или создает короткую ссылку')
     parser.add_argument('url', help='Ссылка для проверки')
     args = parser.parse_args()
+    headers = {"Authorization": f"Bearer {token}"}
     link = args.url
+    short_link = urlparse(link).netloc + urlparse(link).path
+
     try:
-        if link.startswith("https://bit.ly"):
-            bitlink_exist = check_bitlink(token, base_url, link)
+        if is_bitlink(base_url, headers, short_link):
+            bitlink_exist = check_bitlink(headers, base_url, link)
             if bitlink_exist is False:
-                total_clicks = get_total_clicks(token, base_url, link)
+                total_clicks = get_total_clicks(headers, base_url, short_link)
                 print(f"По ссылке {link} перешли {total_clicks} раз(а).")
 
         else:
             bitlink = get_short_link(token, base_url, link)
             print(
-                f"Теперь для доступа к {link} Вы можете воспользоваться следующей ссылкой:\n{bitlink}")
+                "Теперь для доступа к {} воспользуйтесь ссылкой:\n{}".format(
+                    link, bitlink))
     except requests.ConnectionError:
         print("Сайт не отвечает.")
     except KeyError:
