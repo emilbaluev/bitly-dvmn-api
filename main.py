ef8c3dccebf2288929be19b182cd9ec4ab16867b
emilbaluev <emil.baluev@gmail.com>
2021-09-10 01:13:55 +0300

    Put url into functions

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import argparse
 from urllib.parse import urlparse
 
 
-def get_short_link(token, base_url, url_link):
-    url = f'{base_url}/bitlinks'
+def get_short_link(token, url_link):
+    url = 'https://api-ssl.bitly.com/v4//bitlinks'
     headers = {
         'Authorization': f'Bearer {token}',
         'Content-Type': 'application/json',
@@ -18,22 +18,21 @@ def get_short_link(token, base_url, url_link):
 
 
 def get_total_clicks(headers, base_url, short_link):
-    click_url = f'{base_url}/bitlinks/{short_link}/clicks/summary'
+    click_url = f'https://api-ssl.bitly.com/v4//bitlinks/{short_link}/clicks/summary'
     params = {'unit': 'day', 'units': '-1'}
     click_response = requests.get(click_url, headers=headers, params=params)
     click_response.raise_for_status()
     return click_response.json()['total_clicks']
 
 
-def is_bitlink(base_url, headers, short_link):
-    response = requests.get(
-        "{}/bitlinks/{}".format(base_url, short_link),
-        headers=headers)
+def is_bitlink(headers, short_link):
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{short_link}'
+    response = requests.get(url, headers=headers)
     return response.ok
 
 
-def check_bitlink(headers, base_url, link):
-    bitlink_check_url = f"{base_url}bitlinks/{link}"
+def check_bitlink(headers, link):
+    bitlink_check_url = f"https://api-ssl.bitly.com/v4/bitlinks/{link}"
 
     response = requests.get(bitlink_check_url, headers=headers)
     return response.ok
@@ -42,7 +41,6 @@ def check_bitlink(headers, base_url, link):
 def main():
     load_dotenv()
     token = os.getenv("ACCESS_TOKEN")
-    base_url = 'https://api-ssl.bitly.com/v4/'
     parser = argparse.ArgumentParser(
         description='Cчитает количество кликов или создает короткую ссылку')
     parser.add_argument('url', help='Ссылка для проверки')
@@ -52,14 +50,14 @@ def main():
     short_link = urlparse(link).netloc + urlparse(link).path
 
     try:
-        if is_bitlink(base_url, headers, short_link):
-            bitlink_exist = check_bitlink(headers, base_url, link)
+        if is_bitlink(headers, short_link):
+            bitlink_exist = check_bitlink(headers, link)
             if bitlink_exist is False:
-                total_clicks = get_total_clicks(headers, base_url, short_link)
+                total_clicks = get_total_clicks(headers, short_link)
                 print(f"По ссылке {link} перешли {total_clicks} раз(а).")
 
         else:
-            bitlink = get_short_link(token, base_url, link)
+            bitlink = get_short_link(token, link)
             print(
                 "Теперь для доступа к {} воспользуйтесь ссылкой:\n{}".format(
                     link, bitlink))
